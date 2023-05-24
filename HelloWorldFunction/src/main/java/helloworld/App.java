30be04b486ac46e2cc1d7ebd9aa986512e6e4c60
poliana.sato <poliana.sato@vdx.tv>
2023-05-25 00:38:12 +0100

    Tidy up

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -1,6 +1,5 @@
 package helloworld;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,7 +8,6 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.S3Object;
@@ -22,7 +20,6 @@ import com.groupdocs.metadata.core.IReadOnlyList;
 import com.groupdocs.metadata.core.MetadataProperty;
 import com.groupdocs.metadata.search.FallsIntoCategorySpecification;
 import com.groupdocs.metadata.tagging.Tags;
-import com.tests.*;
 
 /**
  * Handler for requests to Lambda function.
@@ -40,7 +37,6 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
             final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_WEST_2).build();
             S3Object o = s3.getObject("shell-test-lambda", "input.mkv");
             S3ObjectInputStream s3is = o.getObjectContent();
-            System.out.println(input.getBody() + "body");
             IReadOnlyList<MetadataProperty> properties = this.getMetadata(s3is);
             String output = String.format("{ \"message\": \"Metadata properties\":  }", properties);
             
@@ -60,11 +56,10 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
             if (metadata.getFileFormat() != FileFormat.Unknown && !metadata.getDocumentInfo().isEncrypted()) {
                 System.out.println("The metadata properties describing some characteristics of the file content: title, keywords, language, etc.");
                 IReadOnlyList<MetadataProperty> properties = metadata.findProperties(new FallsIntoCategorySpecification(Tags.getContent()));
-                System.out.println("Metadata properties: " + properties);
-                return properties;
-                // for (MetadataProperty property : properties) {
-                //     System.out.println(String.format("Property name: %s, Property value: %s", property.getName(), property.getValue()));
-                // }
+                for (MetadataProperty property : properties) {
+                    System.out.println(String.format("Property name: %s, Property value: %s", property.getName(), property.getValue()));
+                }
+                return null;
             }
         }
         return null;
